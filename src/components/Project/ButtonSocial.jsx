a04b87954255296fa0b4ffc089d89d1a9eaa4877
Flavio Rodriguez <ihaio95@gmail.com>
2022-10-21 00:16:30 -0300

    first changes projects section

---
+++ src/components/Project/ButtonSocial.jsx
@@ -0,0 +1,17 @@
+import { FaGithub } from "react-icons/fa";
+import { TbWorld } from "react-icons/tb";
+import { Card, Badge } from "react-bootstrap";
+
+function ButtonSocial() {
+  return (
+    <div>
+      <Card.Link className="" href="#" target="_blank">
+        <FaGithub className="buttonSocial" size={30} />
+      </Card.Link>
+      <Card.Link href="#">
+        <TbWorld className="buttonSocial" size={30} />
+      </Card.Link>
+    </div>
+  );
+}
+export default ButtonSocial;
--- src/components/Project/Projects.jsx
+++ src/components/Project/Projects.jsx
@@ -1,73 +1,120 @@
-import { Container, Card, Col, Row } from "react-bootstrap";
+import { Container, Card, Col, Row, Fade, Button } from "react-bootstrap";
+import { useState } from "react";
 
 import Chat from "../../assets/img/Chat.png";
 import Notas from "../../assets/img/Notas.png";
 import Api from "../../assets/img/API.png";
 import "./project.css";
+import ButtonSocial from "./ButtonSocial";
 
 function Projects({ dark }) {
+  const [open, setOpen] = useState(false);
+  const [open1, setOpen1] = useState(false);
+  const [open2, setOpen2] = useState(false);
+
   return (
     <Container fluid className={dark ? "bg-dark text-white py-5" : "py-5"}>
       <hr />
       <h4 id="Projects" className="fs-1 fw-bold text-center py-5 mb-5">
         Mis proyectos personales
       </h4>
-      <Row xs={1} sm={2} md={2} className="mx-md-3 g-4 justify-content-center">
+      <Row
+        xs={1}
+        sm={2}
+        md={2}
+        lg={3}
+        className="g-4 mx-md-3 justify-content-center"
+      >
         <Col>
-          <Card className="portfolio__item text-white">
-            <Card.Img src={Chat} alt="Chat image" className="portfolio__img" />
-            <div className="portfolio__description">
-              <Card.Title className="portfolio__description--title">
-                Chat
+          <Card border={!dark && "dark"} className="h-100">
+            <Card.Img variant="top" className="img" src={Chat} />
+            <Card.Body className={dark && "bg-darkCustom"}>
+              <Card.Title className="d-flex justify-content-around">
+                <div>Chat</div>
               </Card.Title>
-              <Card.Text className="portfolio__description--text">
-                Se creeo una aplicacion real-time usando sockets.io, Node.js,
-                Html, Css, JS y MongoDB con Mongoose.
-              </Card.Text>
-              <div class="buttons__container">
-                <a
-                  class="button"
-                  href="https://app-firstchattest.herokuapp.com/"
-                  target="_blank"
-                  rel="noopener noreferrer"
+              <div className="d-flex justify-content-between">
+                <Button
+                  variant={
+                    dark
+                      ? "outline-light buttonSelectedLight"
+                      : "outline-dark buttonSelectedDark"
+                  }
+                  onClick={() => setOpen(!open)}
+                  aria-controls="example-fade-text"
+                  aria-expanded={open}
                 >
-                  Website
-                </a>
-                <a
-                  class="button"
-                  href="https://github.com/Flavio-RF/Chat"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Repository
-                </a>
+                  Ver mas detalles
+                </Button>
+                <ButtonSocial />
               </div>
-            </div>
-          </Card>
-        </Col>
-        <Col>
-          <Card>
-            <Card.Img variant="top" src={Notas} />
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </Card.Text>
+              <Fade in={open}>
+                <div id="example-fade-text">
+                  Se creeo una aplicacion real-time usando sockets.io, Node.js,
+                  Html, Css, JS y MongoDB con Mongoose.
+                </div>
+              </Fade>
             </Card.Body>
           </Card>
         </Col>
         <Col>
-          <Card>
-            <Card.Img variant="top" src={Api} />
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </Card.Text>
+          <Card border={!dark && "dark"} className="h-100">
+            <Card.Img variant="top" className="img" src={Notas} />
+            <Card.Body className={dark && "bg-darkCustom"}>
+              <Card.Title className="d-flex justify-content-around">
+                <div>Notas</div>
+              </Card.Title>
+              <div className="d-flex justify-content-between">
+                <Button
+                  variant={
+                    dark
+                      ? "outline-light buttonSelectedLight"
+                      : "outline-dark buttonSelectedDark"
+                  }
+                  onClick={() => setOpen1(!open1)}
+                  aria-controls="example-fade-text"
+                  aria-expanded={open1}
+                >
+                  Ver mas detalles
+                </Button>
+                <ButtonSocial />
+              </div>
+              <Fade in={open1}>
+                <div id="example-fade-text">
+                  Se creeo un CRUD de notas utilizando sockets.io, Node.js,
+                  Html, Css, JS y MongoDB con Mongoose.
+                </div>
+              </Fade>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card border={!dark && "dark"} className="h-100">
+            <Card.Img variant="top" className="img" src={Api} />
+            <Card.Body className={dark && "bg-darkCustom"}>
+              <Card.Title className="d-flex justify-content-around">
+                <div>API</div>
+              </Card.Title>
+              <div className="d-flex justify-content-between">
+                <Button
+                  variant={
+                    dark
+                      ? "outline-light buttonSelectedLight"
+                      : "outline-dark buttonSelectedDark"
+                  }
+                  onClick={() => setOpen2(!open2)}
+                  aria-controls="example-fade-text"
+                  aria-expanded={open2}
+                >
+                  Ver mas detalles
+                </Button>
+                <ButtonSocial />
+              </div>
+              <Fade in={open2}>
+                <div id="example-fade-text">
+                  La idea es poder consumir la API desde Front-end, se trata de
+                  solo Back-end con Node.js, Express, MongoDB, Mongoose.
+                </div>
+              </Fade>
             </Card.Body>
           </Card>
         </Col>
