ed6250df602ce8e5abedf0079323d5ae86647455
Flavio Rodriguez <ihaio95@gmail.com>
2022-10-22 16:58:14 -0300

    code upgrade

--- src/components/About/Icons.jsx
+++ src/components/About/Icons.jsx
@@ -1,53 +1,83 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import { FaReact } from "react-icons/fa";
 import { DiNodejs } from "react-icons/di";
-import { SiExpress, SiJavascript } from "react-icons/si";
-import { FaGithub, FaGitAlt } from "react-icons/fa";
-import { SiSequelize, SiMysql, SiMongodb } from "react-icons/si";
+import { FaGithub, FaGitAlt, FaReact } from "react-icons/fa";
+import {
+  SiSequelize,
+  SiMysql,
+  SiMongodb,
+  SiExpress,
+  SiJavascript,
+} from "react-icons/si";
 import "./icons.css";
+
+const icons = [
+  {
+    id: 1,
+    icon: <FaReact size={70} style={{ color: "#02327a" }} />,
+    title: "React",
+  },
+  {
+    id: 2,
+    icon: <DiNodejs size={70} style={{ color: "green" }} />,
+    title: "Node",
+  },
+  {
+    id: 3,
+    icon: <SiJavascript size={70} style={{ color: "#d1d100bd" }} />,
+    title: "Javascript",
+  },
+  {
+    id: 4,
+    icon: <SiExpress size={70} style={{ color: "black" }} />,
+    title: "Express",
+  },
+  {
+    id: 5,
+    icon: <FaGitAlt size={70} style={{ color: "orange" }} />,
+    title: "Git",
+  },
+  {
+    id: 6,
+    icon: <FaGithub size={70} style={{ color: "black" }} />,
+    title: "Github",
+  },
+  {
+    id: 7,
+    icon: <SiMongodb size={70} style={{ color: "green" }} />,
+    title: "Mongodb",
+  },
+  {
+    id: 8,
+    icon: <SiMysql size={70} style={{ color: "#004bbb" }} />,
+    title: "Mysql",
+  },
+  {
+    id: 9,
+    icon: <SiSequelize size={70} style={{ color: "#02327a" }} />,
+    title: "Sequelize",
+  },
+];
 
 function Icons({ dark }) {
   return (
     <Container className="mt-5">
       <Row xs={3} sm={3} md={3} lg={3} className="justify-content-center gap-4">
-        <Col lg={3} md={3} sm={3} className={dark ? "iconDark" : "iconLight"}>
-          <FaReact size={70} style={{ color: "#02327a" }} />
-          <p>React</p>
-        </Col>
-        <Col lg={3} md={3} sm={3} className={dark ? "iconDark" : "iconLight"}>
-          <DiNodejs size={70} style={{ color: "green" }} />
-          <p>Node</p>
-        </Col>
-        <Col lg={3} md={3} sm={3} className={dark ? "iconDark" : "iconLight"}>
-          <SiJavascript size={70} style={{ color: "#d1d100bd" }} />
-          <p>Javascript</p>
-        </Col>
-        <Col lg={3} md={3} sm={3} className={dark ? "iconDark" : "iconLight"}>
-          <SiExpress size={70} style={{ color: "black" }} />
-          <p>Express</p>
-        </Col>
-        <Col lg={3} md={3} sm={3} className={dark ? "iconDark" : "iconLight"}>
-          <FaGitAlt size={70} style={{ color: "orange" }} />
-          <p>Git</p>
-        </Col>
-        <Col lg={3} md={3} sm={3} className={dark ? "iconDark" : "iconLight"}>
-          <FaGithub size={70} style={{ color: "black" }} />
-          <p>Github</p>
-        </Col>
-        <Col lg={3} md={3} sm={3} className={dark ? "iconDark" : "iconLight"}>
-          <SiMongodb size={70} style={{ color: "green" }} />
-          <p>Mongodb</p>
-        </Col>
-        <Col lg={3} md={3} sm={3} className={dark ? "iconDark" : "iconLight"}>
-          <SiMysql size={70} style={{ color: "#004bbb" }} />
-          <p>Mysql</p>
-        </Col>
-        <Col lg={3} md={3} sm={3} className={dark ? "iconDark" : "iconLight"}>
-          <SiSequelize size={70} style={{ color: "#02327a" }} />
-          <p>Sequelize</p>
-        </Col>
+        {icons.map(({ id, icon, title }) => {
+          return (
+            <Col
+              key={id}
+              lg={3}
+              md={3}
+              sm={3}
+              className={dark ? "iconDark" : "iconLight"}
+            >
+              {icon}
+              <p>{title}</p>
+            </Col>
+          );
+        })}
       </Row>
     </Container>
   );
